5a9be78c6a898245e34cf787207d32d021323010
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-12-03 15:43:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_01_For__Soma_dos_Números_Pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 2; i <= N; i += 2)
+            {
+                soma += i;
+            }
+
+            Console.WriteLine($"A soma dos números pares de 1 a {N} é: {soma}");
+
+            Console.ReadKey();
+        }
+    }
+}
+    
